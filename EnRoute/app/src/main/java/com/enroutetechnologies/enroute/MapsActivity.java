7d89b7095d98ad39861132ac7cf7e1bd94fb3efc
Sanhar Balachandran <s6balach@uwaterloo.ca>
2016-11-12 15:14:41 -0500

    button to submit

--- EnRoute/app/src/main/java/com/enroutetechnologies/enroute/MapsActivity.java
+++ EnRoute/app/src/main/java/com/enroutetechnologies/enroute/MapsActivity.java
@@ -12,18 +12,26 @@ import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.provider.DocumentsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.ArrayList;
+
+import org.w3c.dom.Document;
 
 import static com.enroutetechnologies.enroute.R.id.map;
 
@@ -57,7 +65,10 @@ public class MapsActivity extends AppCompatActivity
     private EditText mSearch;
     private SupportMapFragment mMapFragment;
     private GMapV2Direction mGMapV2Direction;
-
+    private Place mFromPlace;
+    private Place mToPlace;
+    private Button mButton;
+    private Document mDocument;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,10 +78,12 @@ public class MapsActivity extends AppCompatActivity
         mFrom = (EditText)findViewById(R.id.from);
         mTo = (EditText)findViewById(R.id.to);
         mSearch = (EditText)findViewById(R.id.search);
+        mButton = (Button)findViewById(R.id.submit);
 
         mFrom.setOnClickListener(onClickListener);
         mTo.setOnClickListener(onClickListener);
         mSearch.setOnClickListener(onClickListener);
+        mButton.setOnClickListener(onClickListener);
 
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(map);
         mMapFragment.getMapAsync(this);
@@ -82,15 +95,22 @@ public class MapsActivity extends AppCompatActivity
             switch(v.getId()){
                 case R.id.from:
                     txtnumber = 0;
+                    autoComplete();
                     break;
                 case R.id.to:
                     txtnumber = 1;
+                    autoComplete();
                     break;
                 case R.id.search:
                     txtnumber = 2;
                     break;
+                case R.id.submit:
+                    if(mFromPlace != null && mToPlace != null){
+//                        mDocument = mGMapV2Direction.getDocument(mFromPlace.getLatLng(),mToPlace.getLatLng(),mGMapV2Direction.MODE_DRIVING);
+                        mapDirection();
+                    }
+                    break;
             }
-            autoComplete();
         }
     };
 
@@ -107,15 +127,30 @@ public class MapsActivity extends AppCompatActivity
         }
     }
 
+    private void mapDirection(){
+        Document doc = mGMapV2Direction.getDocument(mFromPlace.getLatLng(), mToPlace.getLatLng(),
+                GMapV2Direction.MODE_DRIVING);
+
+        ArrayList<LatLng> directionPoint = mGMapV2Direction.getDirection(doc);
+        PolylineOptions rectLine = new PolylineOptions().width(3).color(R.color.Red);
+
+        for (int i = 0; i < directionPoint.size(); i++) {
+            rectLine.add(directionPoint.get(i));
+        }
+        Polyline polylin = mMap.addPolyline(rectLine);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 Place place = PlaceAutocomplete.getPlace(this, data);
                 if (txtnumber == 0){
-                    mFrom.setText(place.getName());
+                    mFromPlace = place;
+                    mFrom.setText(mFromPlace.getName());
                 } else {
-                    mTo.setText(place.getName());
+                    mToPlace = place;
+                    mTo.setText(mToPlace.getName());
                 }
                 mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName().toString()));
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(place.getLatLng()).zoom(14.0f).build();
